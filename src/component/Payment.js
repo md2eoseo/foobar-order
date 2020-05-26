d53f3735f30c7defd8c1de5e7d5983e75e566c09
md2eoseo <md2eoseo@gmail.com>
2020-05-26 14:34:04 +0200

    move onto each payment detail page

--- src/component/Payment.js
+++ src/component/Payment.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import OrderInfo from "./OrderInfo";
 import mobilepayImg from "../images/payment/m-pay.png";
 import creditcardImg from "../images/payment/c-card.png";
@@ -13,6 +13,32 @@ import {
 function Payment({ orders, getLabelByName, hidePayment }, ref) {
   // https://stackoverflow.com/questions/53561913/react-forwarding-multiple-refs
   const { refPayment, refPaymentSummary } = ref;
+  const refPaymentBtns = useRef(null);
+  const refBackToSelectBtn = useRef(null);
+  const refPaymentDetailMobilepay = useRef(null);
+  const refPaymentDetailCreditcard = useRef(null);
+  const refPaymentDetailCash = useRef(null);
+
+  function goToPaymentDetail(payment) {
+    refPaymentBtns.current.classList.add("hidden");
+    refBackToSelectBtn.current.classList.remove("hidden");
+
+    if (payment === "mobilepay") {
+      refPaymentDetailMobilepay.current.classList.remove("hidden");
+    } else if (payment === "creditcard") {
+      refPaymentDetailCreditcard.current.classList.remove("hidden");
+    } else if (payment === "cash") {
+      refPaymentDetailCash.current.classList.remove("hidden");
+    }
+  }
+
+  function goBackToSelect() {
+    refPaymentBtns.current.classList.remove("hidden");
+    refBackToSelectBtn.current.classList.add("hidden");
+    refPaymentDetailMobilepay.current.classList.add("hidden");
+    refPaymentDetailCreditcard.current.classList.add("hidden");
+    refPaymentDetailCash.current.classList.add("hidden");
+  }
 
   return (
     <>
@@ -20,19 +46,52 @@ function Payment({ orders, getLabelByName, hidePayment }, ref) {
         <button className="backToMainBtn" onClick={hidePayment}>
           <FontAwesomeIcon icon={faLongArrowAltLeft} />
         </button>
+        <button
+          ref={refBackToSelectBtn}
+          className="backToSelectBtn hidden"
+          onClick={goBackToSelect}
+        >
+          <FontAwesomeIcon icon={faLongArrowAltLeft} />
+        </button>
         <div className="paymentText">Select the Payment.</div>
-        <div className="paymentBtns">
-          <button className="paymentBtn mobilepay">
+        <div ref={refPaymentBtns} className="paymentBtns">
+          <button
+            className="paymentBtn mobilepay"
+            onClick={() => goToPaymentDetail("mobilepay")}
+          >
             <img src={mobilepayImg} alt="m-pay" />
           </button>
-          <button className="paymentBtn creditcard">
+          <button
+            className="paymentBtn creditcard"
+            onClick={() => goToPaymentDetail("creditcard")}
+          >
             <img src={creditcardImg} alt="c-card" />
             <div>Credit Card</div>
           </button>
-          <button className="paymentBtn cash">
+          <button
+            className="paymentBtn cash"
+            onClick={() => goToPaymentDetail("cash")}
+          >
             <img src={cashImg} alt="cash" />
             <div>Cash</div>
           </button>
+        </div>
+        <div className="paymentDetails">
+          <div
+            ref={refPaymentDetailMobilepay}
+            className="paymentDetail mobilepay hidden"
+          >
+            mobilepay
+          </div>
+          <div
+            ref={refPaymentDetailCreditcard}
+            className="paymentDetail creditcard hidden"
+          >
+            creditcard
+          </div>
+          <div ref={refPaymentDetailCash} className="paymentDetail cash hidden">
+            cash
+          </div>
         </div>
       </div>
       <div ref={refPaymentSummary} className="paymentSummary">
