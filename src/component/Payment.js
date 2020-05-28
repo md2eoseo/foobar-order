08928597a1c75e134101a2987f08849351794f29
md2eoseo <md2eoseo@gmail.com>
2020-05-28 16:53:45 +0200

    added creditcard payment

--- src/component/Payment.js
+++ src/component/Payment.js
@@ -1,4 +1,7 @@
-import React, { useRef } from "react";
+import React, { useState, useRef } from "react";
+import Cards from "react-credit-cards";
+import "react-credit-cards/es/styles-compiled.css";
+import Cleave from "cleave.js/react";
 import OrderInfo from "./OrderInfo";
 import mobilepayImg from "../images/payment/m-pay.png";
 import creditcardImg from "../images/payment/c-card.png";
@@ -15,6 +18,13 @@ function Payment(
   { orders, getLabelByName, hidePayment, completePayment },
   ref
 ) {
+  const [cvc, setCVC] = useState("");
+  const [expiry, setExpiry] = useState("");
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+  const [isNumberValid, setIsNumberValid] = useState(false);
+  const [focus, setFocus] = useState("");
+
   // https://stackoverflow.com/questions/53561913/react-forwarding-multiple-refs
   const { refPayment, refPaymentSummary } = ref;
   const refPaymentBtns = useRef(null);
@@ -24,6 +34,18 @@ function Payment(
   const refPaymentDetailMobilepay = useRef(null);
   const refPaymentDetailCreditcard = useRef(null);
   const refPaymentDetailCash = useRef(null);
+
+  const handleInputFocus = (e) => {
+    setFocus(e.target.name);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "cvc") setCVC(value);
+    else if (name === "expiry") setExpiry(value);
+    else if (name === "name") setName(value.toUpperCase());
+    else if (name === "number") setNumber(value);
+  };
 
   function goToPaymentDetail(payment) {
     refPaymentBtns.current.classList.add("hidden");
@@ -48,6 +70,28 @@ function Payment(
     refPaymentDetailCash.current.classList.add("hidden");
     refPaymentText.current.classList.remove("hidden");
     refPaymentDetails.current.classList.add("hidden");
+  }
+
+  function isValidCreditcard(e) {
+    e.preventDefault();
+    let isValid = true;
+    // if (!isNumberValid) {
+    //   console.log("Card Number Validation Error!");
+    //   isValid = false;
+    // }
+    // if (name.length === 0) {
+    //   console.log("Name Validation Error!");
+    //   isValid = false;
+    // }
+    // if (expiry.length !== 5) {
+    //   console.log("Expiry Date Validation Error!");
+    //   isValid = false;
+    // }
+    // if (cvc.length !== 3) {
+    //   console.log("CVC Validation Error!");
+    //   isValid = false;
+    // }
+    return isValid;
   }
 
   return (
@@ -99,7 +143,74 @@ function Payment(
             ref={refPaymentDetailCreditcard}
             className="paymentDetail creditcard hidden"
           >
-            creditcard
+            <Cards
+              cvc={cvc}
+              expiry={expiry}
+              focus={focus}
+              name={name}
+              number={number}
+              callback={(type, isValid) => {
+                setIsNumberValid(isValid);
+              }}
+            />
+            <form>
+              <Cleave
+                type="tel"
+                name="number"
+                value={number}
+                placeholder="Card Number"
+                options={{ creditCard: true }}
+                onChange={handleInputChange}
+                onFocus={handleInputFocus}
+              />
+              <input
+                name="name"
+                value={name}
+                placeholder="Name"
+                onChange={handleInputChange}
+                onFocus={handleInputFocus}
+              />
+              <div>
+                <Cleave
+                  className="inputExpiry"
+                  type="tel"
+                  name="expiry"
+                  value={expiry}
+                  placeholder="Expiry Date (MM/YY)"
+                  options={{
+                    date: true,
+                    datePattern: ["m", "y"],
+                  }}
+                  onChange={handleInputChange}
+                  onFocus={handleInputFocus}
+                />
+                <input
+                  className="inputCVC"
+                  type="tel"
+                  name="cvc"
+                  value={cvc}
+                  placeholder="CVC"
+                  maxLength="3"
+                  onChange={handleInputChange}
+                  onFocus={handleInputFocus}
+                />
+              </div>
+              <input
+                className="submitBtn"
+                type="submit"
+                value="Complete the Payment"
+                onClick={(e) => {
+                  if (isValidCreditcard(e)) {
+                    completePayment();
+                    goBackToSelect();
+                    setNumber("");
+                    setName("");
+                    setExpiry("");
+                    setCVC("");
+                  }
+                }}
+              />
+            </form>
           </div>
           <div ref={refPaymentDetailCash} className="paymentDetail cash hidden">
             <div className="text">Are you sure paying with cash?</div>
